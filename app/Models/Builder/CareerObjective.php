da01c6b951418717cbd73a0aedbf5c6acd277abe
algoslab <com.algoslab@gmail.com>
2024-07-13 21:30:25 +0600

    Created 12 model for builder

---
+++ app/Models/Builder/CareerObjective.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Builder;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CareerObjective extends Model
+{
+    use HasFactory;
+}
